438e81335c2806c0808cf315ea8a68d693b4c1f9
Salah Ashraf <salahashraf924@gmail.com>
2022-01-10 01:26:54 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+import os
+from pathlib import Path
+links= []
+link_to_download= ""
+numberOfVideos=int(input("Enter the number of videos : "))
+
+for i in range(0,numberOfVideos):
+    link_to_download=input("Enter link here: ")
+    links.append(link_to_download)
+
+for i in range(0,numberOfVideos):
+    link_to_download =links[i]
+    url = YouTube(link_to_download)
+    print("downloading...."+str(i+1))
+    video = url.streams.get_highest_resolution()
+    path_to_download_folder = str(os.path.join(Path.home(), r"")) #enter here the path that you want to save in it
+    video.download(path_to_download_folder)
+    print("Downloaded! :)")
